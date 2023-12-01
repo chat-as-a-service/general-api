b2695f273c591e959925b916ab68bbeaab2bde89
Daniel Kim <yuki9447@colorado.edu>
2023-11-30 19:41:16 -0700

    Deleted unecessary files

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,8 +1,3 @@
 from fastapi import APIRouter
 
 router = APIRouter()
-
-
-@router.post("/session_tokens")
-async def create_account(dto: AccountCreate, db: Session = Depends(get_db)):
-    return await account_service.create_account(dto, db)
