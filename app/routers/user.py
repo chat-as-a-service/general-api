bb9d6333bb998f3fe971fc7e0c523e6da5e79f6e
Jusung Kim <itmekjs@gmail.com>
2023-12-02 11:18:05 -0700

    delete unnecessary comment

--- app/routers/user.py
+++ app/routers/user.py
@@ -12,8 +12,6 @@ router = APIRouter()
 async def create_user(dto: UserCreate, db: Session = Depends(get_db)):
     return await account_service.create_user(dto, db)
 
-# 유저를 만들고 - 데이터베이스에 저장하고 -
-
-홍 내가 생각하는게 맞는지 확인 좀 해줘
-
-유저를 만들고 - 데이터베이스에 저장하고 
+# Compare this snippet from app/routers/user.py:
+# from fastapi import APIRouter, Depends
+# from sqlalchemy.orm import Session
