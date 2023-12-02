dae5b1ba79f2e5261a7dd891b8c06f7588ab975b
MAX <97825248+momoaolig@users.noreply.github.com>
2023-12-01 18:06:36 -0700

    organization view feature updated

--- app/services/application.py
+++ app/services/application.py
@@ -1,6 +1,8 @@
-from sqlalchemy.orm import Session
-
 from app.repositories.account import get_by_api_token
+from sqlalchemy.orm import Session
+from app.models.application import Application
+from app.repositories.application import get_by_name, get_all_applications
+from app.schemas.application import ApplicationCreate
 
 
 async def check_api_token(api_token: str, db: Session):
@@ -8,3 +10,30 @@ async def check_api_token(api_token: str, db: Session):
     if not application:
         return False
     return True
+
+
+async def application_create(dto: ApplicationCreate, db: Session):
+    if get_by_name(db, dto.email):
+        return {"error": "application name already exists"}
+
+    new_application = Application(
+        name=dto.name,
+        created_by="system",
+        updated_by="system",
+    )
+    db.add(new_application)
+    db.commit()
+    db.refresh(new_application)
+    return new_application
+
+
+async def application_view(dto: ApplicationCreate, db: Session):
+    if not get_by_name(db, dto.name):
+        return {"error": "the name does not exist"}
+
+    return get_by_name(db, dto.name)
+
+
+async def application_list(db: Session):
+    applications = get_all_applications(db)
+    return applications
