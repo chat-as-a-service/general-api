f64ad6e32db400b5e51a9b9ce434e5d2e21dcf49
Daniel Kim <dkim331@gmail.com>
2023-12-10 01:48:04 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,8 +2,6 @@ from dotenv import load_dotenv
 load_dotenv()
 import logging  # noqa: F401, E402
 from fastapi import FastAPI  # noqa: E402
-from app.routers import user # noqa: E402
-from .routers import account, organization  # noqa: E402
 from fastapi.middleware.cors import CORSMiddleware  # noqa: E402
 
 app = FastAPI()
@@ -14,7 +12,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-from .routers import account, channel, organization, application, attachment  # noqa: E402
+from .routers import user, account, channel, organization, application, attachment  # noqa: E402
 
 app.include_router(account.router, prefix="/accounts", tags=["accounts"])
 app.include_router(attachment.router, prefix="/attachments", tags=["attachments"])
